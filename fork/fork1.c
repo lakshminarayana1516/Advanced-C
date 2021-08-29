d2bfb29eee6aa6ab50f8e9598b80cfbc41317b5d
lakshminarayana1516 <narayana8522@gmail.com>
2021-08-29 18:34:44 +0530

    my first fork file

--- fork/fork1.c
+++ fork/fork1.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-	printf("before fork....\n");
-	fork();
-	printf("after fork...\n");
+	printf("before fork....\n");  //it will print only one time by the parent process
+	fork();     //creating a child process using the fork.
+	printf("after fork...\n");    // this will print two times once by child and once by parent
 }
 
